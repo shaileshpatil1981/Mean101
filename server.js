dfc33d82e3d154d54b48bb3d6db4f2fab3f1305c
shaileshpatil1981 <shailesh.patil3@cognizant.com>
2017-05-11 07:32:23 +0000

    11-May Assignment

--- server.js
+++ server.js
@@ -3,9 +3,26 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
 mongoose.connect('mongodb://localhost:27017/mittens');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mittens');
+
+var Meow = mongoose.model('Meow', {
+  text: string
+});
+
+app.get('/meows', function(req,res,next){
+  Meow.find({}, function(err, meows){
+    return res.json(meows);
+  });
+
+  
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+});
+
 });
 
