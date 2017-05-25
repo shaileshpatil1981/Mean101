f8c834366d92c735ea229ff751a61987a315e34a
shaileshpatil1981 <shailesh.patil3@cognizant.com>
2017-05-25 06:29:22 +0000

    Changes for Session 4

--- server.js
+++ server.js
@@ -3,26 +3,30 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
-app.use(express.static('public'));
-
+app.use(express.static('public')); 
 mongoose.connect('mongodb://localhost:27017/mittens');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mittens');
 
 var Meow = mongoose.model('Meow', {
-  text: string
-});
-
-app.get('/meows', function(req,res,next){
-  Meow.find({}, function(err, meows){
-    return res.json(meows);
+ text: String
   });
 
-  
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  app.get('/meows',function(req,res,next){
+    Meow.find({},function(err,meows){
+      return res.json(meows);
+    });
+  })
+  app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+	});
+
+app.listen(4000, function () {
+  console.log('Example : Shailesh app listening on port 4000!')
 });
 
-});
 
